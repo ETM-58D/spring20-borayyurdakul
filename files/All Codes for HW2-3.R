95bc022ee65a926ddf35ac8c2b6d762f9fdeab8d
borayyurdakul <boray.yurdakul@boun.edu.tr>
2020-06-07 23:36:56 +0300

    a

--- files/All Codes for HW2-3.R
+++ files/All Codes for HW2-3.R
@@ -10,7 +10,7 @@ require(tidyr)
 require(glmnet)
 require(ggplot2)
 set.seed(100)
-consumption=fread("Desktop/Okul/II.Dönem/Business Analytics/Homework 2-3/GercekZamanliTuketim-01012016-19052020.csv")
+consumption=fread("/Users/Boray/Documents/school/bogazici/ETM58d/hw2-3/GercekZamanliTuketim-01012016-19052020.csv")
 consumption
 setnames(consumption,names(consumption)[3],'value')
 consumption[,date:=as.Date(Tarih,'%d.%m.%Y')]
